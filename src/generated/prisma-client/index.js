a23a5d531eb08a3497125d149f4afb5b4c534cef
Md Ryad Ahmed Biplob <rb147258@gmail.com>
2019-11-04 01:52:19 +0600

    created prisma client

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Link",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/md-ryad-ahmed-biplob-0b6059/node_graphql_practice/dev`
+});
+exports.prisma = new exports.Prisma();
